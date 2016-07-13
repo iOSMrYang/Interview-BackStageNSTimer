1509f28d88d0938c30ad6ee3bdac909a85d044f3
杨杰 <yangjieljy120601@gmail.com>
2016-07-13 11:37:06 +0800

    1.后台运行计时器 2.plist文件添加相关字段Required background modes

--- Interview-BackStageNSTimer/ViewController.m
+++ Interview-BackStageNSTimer/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong)NSTimer *timer ;
 @end
 
 @implementation ViewController
@@ -17,8 +17,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setupBackgroundHandler];
+    
 }
 
+- (void)setupBackgroundHandler
+{
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(requestServerHowManyUnreadMessages) userInfo:nil repeats:YES];
+    [timer fire];
+    self.timer = timer;
+}
+
+- (void)requestServerHowManyUnreadMessages
+{
+    UIApplication* app = [UIApplication sharedApplication];
+    
+    if([app applicationState] == UIApplicationStateBackground)
+    {
+        NSLog(@"UIApplicationStateBackground");
+    }
+    else if([app applicationState] == UIApplicationStateActive)
+    {
+        NSLog(@"UIApplicationStateActive");
+    }
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
